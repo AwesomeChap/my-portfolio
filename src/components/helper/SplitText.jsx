23bd9ecfc2f439ec456dda4ae34d0852ab89f6dc
Jatin Kumar <jatin15011999@gmail.com>
2026-05-18 22:09:21 +0200

    mobile nav pixel text centerd

--- src/components/helper/SplitText.jsx
+++ src/components/helper/SplitText.jsx
@@ -4,9 +4,13 @@ import '../../styles/SplitText.scss';
 export default (props) => {
   const isActive = props.selected === props.index;
 
-  return (
-    <span className={isActive ? 'nav-label nav-label--active' : 'nav-label'}>
-      {props.name}
-    </span>
-  );
+  if (isActive) {
+    return (
+      <span className="nav-label nav-label--active">
+        <span className="nav-label__pixel-text">{props.name}</span>
+      </span>
+    );
+  }
+
+  return <span className="nav-label">{props.name}</span>;
 };
